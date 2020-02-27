7486b232bd03af981477fbc6de5c86a5dcfcf6ed
segwuonwu <segwuonwu@gmail.com>
2020-02-26 16:21:10 -0800

    Created a model for menu and dayofweek. Ran migration

--- models/dayofweek.js
+++ models/dayofweek.js
@@ -1,12 +1,13 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
-  const dayOfWeek = sequelize.define('dayOfWeek', {
-    name: DataTypes.STRING,
-    time: DataTypes.STRING,
-    menuId: DataTypes.INTEGER
-  }, {});
-  dayOfWeek.associate = function(models) {
-    // associations can be defined here
-  };
-  return dayOfWeek;
+    const dayofweek = sequelize.define('dayofweek', {
+        name: DataTypes.STRING,
+        time: DataTypes.STRING,
+        menuId: DataTypes.INTEGER
+    }, {});
+    dayofweek.associate = function(models) {
+        // associations can be defined here
+        models.dayofweek.hasMany(models.menu);
+    };
+    return dayofweek;
 };
--- models/menu.js
+++ models/menu.js
@@ -1,10 +1,11 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
-  const menu = sequelize.define('menu', {
-    name: DataTypes.STRING
-  }, {});
-  menu.associate = function(models) {
-    // associations can be defined here
-  };
-  return menu;
+    const menu = sequelize.define('menu', {
+        name: DataTypes.STRING
+    }, {});
+    menu.associate = function(models) {
+        // associations can be defined here
+        models.menu.belongsTo(models.dayofweek);
+    };
+    return menu;
 };
